71d6ffd13df0a220932908f4531bd69d86f1c939
gogeorge <grvaltas@gmail.com>
2024-02-08 12:41:18 +0100

    mvp commit

---
+++ main.py
@@ -0,0 +1,127 @@
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce
+import logging
+import asyncio
+import config
+from stock_prediction import stockPred
+
+
+# ENABLE LOGGING - options, DEBUG,INFO, WARNING?
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+# Alpaca Trading Client
+trading_client = TradingClient(
+    config.API_KEY, config.API_SECRET, paper=True)
+
+
+# Trading variables
+trading_pair = 'ETH/USD'
+qty_to_trade = 1
+# Wait time between each bar request and training model
+waitTime = 100
+data = 0
+
+current_position, current_price = 0, 0
+predicted_price = 0
+
+
+async def main():
+    '''
+    Main function to get latest asset data and check possible trade conditions
+    '''
+
+    while True:
+        logger.info('----------------------------------------------------')
+
+        pred = stockPred()
+        global predicted_price, current_price
+        predicted_price = pred.predictModel()
+        current_price = pred.getCurrentPrice()
+        logger.info("Predicted Price is {0}".format(predicted_price))
+        l1 = loop.create_task(check_condition())
+        await asyncio.wait([l1])
+        await asyncio.sleep(waitTime)
+
+
+
+async def check_condition():
+    global current_position, current_price, predicted_price
+    current_position = get_positions()
+    logger.info("Current Price is: {0}".format(current_price))
+    logger.info("Current Position is: {0}".format(current_position))
+    # If we do not have a position and current price is less than the predicted price place a market buy order
+    if float(current_position) <= 0.01 and current_price < predicted_price:
+        logger.info("Placing Buy Order")
+        buy_order = await post_alpaca_order('buy', 1)
+        if buy_order:
+            logger.info("Buy Order Placed")
+            print("Balance?: ", current_position)
+
+    # If we do have a position and current price is greater than the predicted price place a market sell order
+    if float(current_position) >= 0.01 and current_price > predicted_price:
+        logger.info("Placing Sell Order")
+        sell_order = await post_alpaca_order('sell', 1)
+        if sell_order:
+            logger.info("Sell Order Placed")
+            print("Balance?: ", current_position)
+    
+    # if we have a position and the current price is really low buy again
+    if float(current_position) >= 0.01 and (predicted_price - current_price)/predicted_price >= 0.1:
+        logger.info("Placing Add. Buy Order")
+        buy_order = await post_alpaca_order('buy', 0.5)
+        if buy_order:
+            logger.info("Buy Order Placed")
+            print("Balance?: ", current_position)
+
+
+async def post_alpaca_order(side, size):
+    '''
+    Post an order to Alpaca
+    '''
+    try:
+        if side == 'buy':
+            market_order_data = MarketOrderRequest(
+                symbol="ETHUSD",
+                qty=size,
+                side=OrderSide.BUY,
+                time_in_force=TimeInForce.GTC
+            )
+            buy_order = trading_client.submit_order(
+                order_data=market_order_data
+            )
+            return buy_order
+        else:
+            market_order_data = MarketOrderRequest(
+                symbol="ETHUSD",
+                qty=size,
+                side=OrderSide.SELL,
+                time_in_force=TimeInForce.GTC
+            )
+            sell_order = trading_client.submit_order(
+                order_data=market_order_data
+            )
+            return sell_order
+    except Exception as e:
+        logger.exception(
+            "There was an issue posting order to Alpaca: {0}".format(e))
+        return False
+
+
+def get_positions():
+    positions = trading_client.get_all_positions()
+    # print(positions[0])
+    global current_position
+    for p in positions:
+        if p.symbol == 'ETHUSD':
+            current_position = p.qty
+            return current_position
+    return current_position
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
---
+++ stock_prediction.py
@@ -0,0 +1,208 @@
+from alpaca.data.historical import CryptoHistoricalDataClient
+from alpaca.data.requests import CryptoBarsRequest
+from alpaca.data.timeframe import TimeFrame
+from alpaca_trade_api import REST
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from timedelta import Timedelta 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Dropout, LSTM
+from sklearn.preprocessing import MinMaxScaler
+from alpaca.trading.client import TradingClient
+from attention import Attention
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+import json
+from finbert import estimate_sentiment
+import config
+import logging
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+trading_client = TradingClient(
+    config.API_KEY, config.API_SECRET, paper=True)
+
+trading_pair = 'ETH/USD'
+qty_to_trade = 1
+# Wait time between each bar request and training model
+waitTime = 100
+data = 0
+
+current_position, current_price = 0, 0
+predicted_price = 0
+
+class stockPred:
+    def __init__(self,
+                 past_days: int = 50,
+                 trading_pair: str = 'ETHUSD',
+                 exchange: str = 'FTXU',
+                 feature: str = 'close',
+
+                 look_back: int = 100,
+
+                 neurons: int = 50,
+                 activ_func: str = 'linear',
+                 dropout: float = 0.2,
+                 loss: str = 'mse',
+                 optimizer: str = 'adam',
+                 epochs: int = 20,
+                 batch_size: int = 32,
+                 output_size: int = 1
+                 ):
+        self.exchange = exchange
+        self.feature = feature
+
+        self.look_back = look_back
+
+        self.neurons = neurons
+        self.activ_func = activ_func
+        self.dropout = dropout
+        self.loss = loss
+        self.optimizer = optimizer
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.output_size = output_size
+        self.api = REST(base_url=config.BASE_URL, key_id=config.API_KEY, secret_key=config.API_SECRET)
+
+
+    def getAllData(self):
+
+        # Alpaca Market Data Client
+        data_client = CryptoHistoricalDataClient()
+
+        time_diff = datetime.now() - relativedelta(hours=1000)
+        logger.info("Getting bar data for {0} starting from {1}".format(
+            trading_pair, time_diff))
+        # Defining Bar data request parameters
+        request_params = CryptoBarsRequest(
+            symbol_or_symbols=[trading_pair],
+            timeframe=TimeFrame.Hour,
+            start=time_diff
+        )
+        # Get the bar data from Alpaca
+        df = data_client.get_crypto_bars(request_params).df
+        global current_price
+        current_price = df.iloc[-1]['close']
+        return df
+
+    @staticmethod
+    def getCurrentPrice():
+        return current_price
+
+    def getFeature(self, df):
+        data = df.filter([self.feature])
+        data = data.values
+        return data
+
+    def scaleData(self, data):
+        scaler = MinMaxScaler(feature_range=(-1, 1))
+        scaled_data = scaler.fit_transform(data)
+        return scaled_data, scaler
+
+    # train on all data for which labels are available (train + test from dev)
+    def getTrainData(self, scaled_data):
+        x, y = [], []
+        for price in range(self.look_back, len(scaled_data)):
+            x.append(scaled_data[price - self.look_back:price, :])
+            y.append(scaled_data[price, :])
+        return np.array(x), np.array(y)
+    
+    def get_dates(self): 
+        today = datetime.now()
+        three_days_prior = today - Timedelta(days=3)
+        return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
+    
+    def get_sentiment(self): 
+        today, three_days_prior = self.get_dates()
+        news = self.api.get_news(symbol=trading_pair, 
+                                 start=three_days_prior, 
+                                 end=today) 
+        news = [ev.__dict__["_raw"]["headline"] for ev in news]
+        print(news)
+        probability, sentiment = estimate_sentiment(news)
+        return probability, sentiment 
+
+    def LSTM_model(self, input_data):
+        model = Sequential()
+        model.add(LSTM(self.neurons, input_shape=(
+            input_data.shape[1], input_data.shape[2]), return_sequences=True))
+        model.add(Dropout(self.dropout))
+        model.add(LSTM(self.neurons, return_sequences=True))
+        model.add(Dropout(self.dropout))
+        model.add(LSTM(self.neurons))
+        model.add(Attention(self.neurons))
+        model.add(Dropout(self.dropout))
+        model.add(Dense(units=self.output_size))
+        model.add(Activation(self.activ_func))
+
+        model.compile(loss=self.loss, optimizer=self.optimizer)
+        return model
+
+    def trainModel(self, x, y):
+        x_train = x[: len(x) - 1]
+        y_train = y[: len(x) - 1]
+        model = self.LSTM_model(x_train)
+        modelfit = model.fit(x_train, y_train, epochs=self.epochs,
+                             batch_size=self.batch_size, verbose=1, shuffle=True)
+        return model, modelfit
+
+    def predictModel(self):
+        logger.info("Getting Ethereum Bar Data")
+        # get all data
+        df = self.getAllData()
+
+        logger.info("Getting Feature: {}".format(self.feature))
+        # get feature (closing price)
+        data = self.getFeature(df)
+
+        logger.info("Scaling Data")
+        # scale data and get scaler
+        scaled_data, scaler = self.scaleData(data)
+
+        logger.info("Getting Train Data")
+        # get train data
+        x_train, y_train = self.getTrainData(scaled_data)
+
+        logger.info("Training Model")
+        # Creates and returns a trained model
+        model = self.trainModel(x_train, y_train)[0]
+
+        logger.info("Getting News Data")
+
+        prob, sentiment = self.get_sentiment()
+        logger.info(prob)
+        logger.info(sentiment)
+
+        logger.info("Extracting data to predict on")
+        # Extract the Closing prices that need to be fed into predict the result
+        x_pred = scaled_data[-self.look_back:]
+        x_pred = np.reshape(x_pred, (1, x_pred.shape[0]))
+
+        # Predict the result
+        logger.info("Predicting Price")
+        pred = model.predict(x_pred)
+        # fuse
+        bias = 1
+        sentiment_threshold = 0.9
+        if prob > sentiment_threshold:
+            bias = 0.95
+        else:
+            bias = -0.5
+        fuse = (pred.item())*(prob**(1-bias))
+        print('fuse')
+        print(fuse)
+        pred = pred.squeeze()
+        pred = np.array([float(pred)])
+        pred = np.reshape(pred, (pred.shape[0], 1))
+        print('pred rescale')
+        print(pred)
+
+        # Inverse the scaling to get the actual price
+        pred_true = scaler.inverse_transform(pred)
+        return pred_true[0][0]
+
